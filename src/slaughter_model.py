ef5f2fc7532dd5045a8db25b759531bdfc1a1947
Kevin Rassool <kevin.rassool@gmail.com>
2022-09-16 14:46:14 +1000

    working

--- src/slaughter_model.py
+++ src/slaughter_model.py
@@ -1,6 +1,21 @@
 import pandas as pd
 
+
+path    = "../data/NassCattle2022.csv"
+df      = pd.read_csv(path)
+
+df.set_index("Variable",
+            inplace = True)
+
 print("hello world")
+
+
+
+months = 6
+for i in range(months):
+    print(i)
+
+
 
 
 
